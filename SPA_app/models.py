7facab29318576d999886a786ef087fe73a6ec83
duseth <ilyas.salimov.07@gmail.com>
2021-06-25 10:07:16 +0300

    project(models): добавить модели и миграции

--- SPA_app/models.py
+++ SPA_app/models.py
@@ -2,10 +2,29 @@ from django.db import models
 
 
 class Medicines(models.Model):
-    name = models.CharField(max_length=255, blank=True, null=True)
-    photo = models.CharField(max_length=255, blank=True, null=True)
-    price = models.CharField(max_length=20, blank=True, null=True)
-    url = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255)
+    photo = models.URLField(max_length=255)
+    price = models.IntegerField()
+    url = models.URLField(max_length=255)
+    pharmacy = models.CharField(max_length=255)
 
     class Meta:
         db_table = "medicines"
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=255)
+    photo = models.URLField(max_length=255)
+
+    class Meta:
+        db_table = "products"
+
+
+class Pharmacies(models.Model):
+    price = models.IntegerField()
+    url = models.URLField(max_length=255)
+    pharmacy = models.CharField(max_length=255)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "pharmacies"
