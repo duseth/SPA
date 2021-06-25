62adac48c41a7386df1c35d60cd77d3124ddb992
duseth <ilyas.salimov.07@gmail.com>
2021-06-25 16:45:39 +0300

    project(views): добавить предсталения

--- SPA_app/views.py
+++ SPA_app/views.py
@@ -1,21 +1,50 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest
 
 from .models import *
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    if request.method == "POST":
-        return search(request, request.POST.get("q"))
     return render(request, "index.html")
 
 
-def search(request: HttpRequest, query: str):
-    medicines = Medicines.objects.all()
-    query_medicines = list()
+def search(request: HttpRequest) -> HttpResponse:
+    query = request.GET.get("query")
+    sort = request.GET.get("sort", default="name")
+    sort_list: dict = {
+        value: name
+        for value, name in
+        zip(["name", "byprice", "bypricedesc"], ["по алфавиту", "по возрастанию цены", "по убыванию цены"])
+    }
 
-    for medicine in medicines:
+    if query != "":
+        medicines = __get_medicines(query, sort)
+        paginator = Paginator(medicines, 10)
+
+        page_number = request.GET.get("page")
+        page = paginator.get_page(page_number)
+
+        return render(request, "search.html", {"query": query, "sort_list": sort_list.items(), "sort": sort, "page": page})
+    return redirect("SPA_app:index")
+
+
+def pharmacies(request: HttpRequest) -> HttpResponse:
+    return render(request, "pharmacies.html")
+
+
+def contacts(request: HttpRequest) -> HttpResponse:
+    return render(request, "contacts.html")
+
+
+def __get_medicines(query: str, sort: str) -> list:
+    sort_key: dict = dict(name="name", byprice="price", bypricedesc="-price")
+
+    all_medicines = Medicines.objects.order_by(sort_key.get(sort))
+
+    medicines = list()
+    for medicine in all_medicines:
         if query.lower() in medicine.name.lower():
-            query_medicines.append(medicine)
+            medicines.append(medicine)
 
-    return render(request, "search.html", {"medicines": query_medicines[:100]})
+    return medicines
