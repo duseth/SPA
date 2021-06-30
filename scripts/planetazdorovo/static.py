de555b770e6496f97060c64d5a38bed829dfe508
duseth <ilyas.salimov.07@gmail.com>
2021-06-30 14:24:11 +0300

    feat(scripts): добавить скрипт и исправить прошлые

---
+++ scripts/planetazdorovo/static.py
@@ -0,0 +1,25 @@
+DEFAULT_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Referer': 'https://planetazdorovo.ru/',
+    'DNT': '1',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Cache-Control': 'max-age=0',
+    'TE': 'Trailers',
+}
+DEFAULT_COOKIES = {
+    'city_id': '1343',
+    'city_xml': '88',
+    'city': '%D0%A3%D0%BB%D1%8C%D1%8F%D0%BD%D0%BE%D0%B2%D1%81%D0%BA',
+    'help_phone': '%288422%29+790-900',
+    'order_phone': '8+%288422%29+30-32-02',
+    'region': '138',
+    'timezone': '4',
+    'is_order_warning': '1',
+    'city_code': 'ulyanovsk',
+    'IS_CITY_CHANGE': '1',
+}
+BASE_URL = 'https://planetazdorovo.ru'
+CATALOG_URL = BASE_URL + '/catalog/'
