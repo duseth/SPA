041e58f040d00e97b5acd070af4e9eb155ad2a9d
duseth <ilyas.salimov.07@gmail.com>
2021-07-01 10:01:45 +0300

    fix(scripts): исправить скрипты

--- scripts/zdravcity_ru.py
+++ scripts/zdravcity_ru.py
@@ -62,7 +62,6 @@ class Spider(scrapy.Spider):
                          pharmacy='zdravcity').save()
 
 
-
 def run():
     process = CrawlerProcess(settings={
         "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
